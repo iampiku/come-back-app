5229d948575c3686206131a268bbd56c4f0ebc1d
Pradipta Chatterjee <iampikuchatterjee@gmail.com>
2026-02-17 10:00:23 +0530

    feat: set up initial Spring Boot application with PostgreSQL support and Maven wrapper

---
+++ server/src/main/java/com/comeback/app/ComebackApplication.java
@@ -0,0 +1,13 @@
+package com.comeback.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComebackApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/comeback/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeback.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
